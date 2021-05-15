4450e87534d91a6b8ccbb01a424257bac1b5b112
Tommi Virtanen <tv@eagain.net>
2021-05-15 12:23:42 -0600

    nix: Modernize shell.nix

--- shell.nix
+++ shell.nix
@@ -1,41 +1,31 @@
 let
-  moz_overlay = import (builtins.fetchTarball {
-    url = https://github.com/mozilla/nixpkgs-mozilla/archive/8c007b60731c07dd7a052cce508de3bb1ae849b4.tar.gz;
-    sha256 = "1zybp62zz0h077zm2zmqs2wcg3whg6jqaah9hcl1gv4x8af4zhs6";
-  });
+  # avoid pkgs.fetchFromGitHub because with that we'd need to import nixpkgs to construct nixpkgs, and that ends up putting nix into a recursion and aborting. this also means this .nix file won't take an optional `pkgs` argument like most of them do.
+  rustOverlay = (import (builtins.fetchTarball {
+    url = "https://github.com/oxalica/rust-overlay/archive/e88036b9fc7b6ad4e2db86944204877b9090d8b9.tar.gz";
+    sha256 = "1m29m49f7q0r6qvzjxkyq3yqiqff6b4cwl385cbpz551421bmr63";
+  }));
   pkgs = import <nixpkgs> {
-    overlays = [ moz_overlay ];
+    overlays = [ rustOverlay ];
   };
-  rustChannel = pkgs.rustChannelOf { date = "2021-02-14"; channel = "nightly"; };
-  rustcWASM = rustChannel.rust.override {
-    targets = [ "wasm32-unknown-unknown" ];
+  rustc = pkgs.rust-bin.stable."1.52.1".minimal.override {
     extensions = [ "rust-src" "rust-analysis" ];
+    targets = [ "wasm32-unknown-unknown" "x86_64-unknown-linux-musl" ];
   };
-
-  catflap = with pkgs; rustPlatform.buildRustPackage rec {
-    pname = "catflap";
-    version = "1.2.0";
-
-    src = fetchFromGitHub {
-      owner = "passcod";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "077fy2disrixi4s1223flkvlh43wjm51mzv38zls1l0y2ykq0z1y";
-    };
-    cargoSha256 = "1qh74faajjzap2csbn39wq3bpa7yma2073xz19qmlp0l32vvjr1g";
+  rustPlatform = pkgs.makeRustPlatform {
+    cargo = rustc;
+    rustc = rustc;
   };
-
 in
 with pkgs;
 stdenv.mkDerivation {
-  name = "rust";
+  name = "nix-shell-rust";
   buildInputs = [
-    rustcWASM
+    rustc
     cargo
     rustfmt
     rustPackages.clippy
     cargo-edit
-    catflap
+    systemfd
     cargo-watch
     ws
   ];
